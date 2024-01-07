ef873c4b1c940c390cb35bf5a29f6cba811c5285
Thomas Lange <57691168+quadraserver@users.noreply.github.com>
2024-01-07 23:48:18 +0100

    Update 2-configuration.sh

--- 2-configuration.sh
+++ 2-configuration.sh
@@ -6,9 +6,6 @@
 # | |__| (_) | | | |  _| | (_| | |_| | | | (_| | |_| | (_) | | | |
 #  \____\___/|_| |_|_| |_|\__, |\__,_|_|  \__,_|\__|_|\___/|_| |_|
 #                         |___/                                   
-# 
-# original by Stephan Raabe (2023) 
-# recreated by Thomas Lange (2023) 
 # ------------------------------------------------------
 clear
 read -p "Please enter your desired keyboard (i.e. de-latin1): " keyboardlayout
@@ -23,7 +20,6 @@ read -p "Please enter your desired username: " username
 # Set System Time
 # ------------------------------------------------------
 ln -sf /usr/share/zoneinfo/$zoneinfo /etc/localtime
-hwclock --systohc
 
 # ------------------------------------------------------
 # Update reflector
@@ -39,7 +35,7 @@ pacman -Syy
 # ------------------------------------------------------
 # Install Packages
 # ------------------------------------------------------
-pacman --noconfirm -S linux-headers efibootmgr dosfstools gptfdisk ntfs-3g os-prober grub grub-btrfs networkmanager network-manager-applet nm-connection-editor firefox firefox-i18n-de thunderbird thunderbird-i18n-de acpid acpi acpi_call dbus dialog wpa_supplicant mtools avahi nfs-utils inetutils dnsmasq openbsd-netcat ipset firewalld flatpak sof-firmware dnsutils xdg-desktop-portal-wlr xdg-user-dirs xdg-utils gvfs gvfs-smb bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion terminus-font htop neofetch mc zip unzip xarchiver p7zip nss-mdns exa bat duf xorg xorg-xinit xclip xf86-video-amdgpu xf86-video-nouveau xf86-video-intel xf86-video-qxl brightnessctl pacman-contrib inxi lvm2 wget git gcc ruby go ranger xorg-server libreoffice-fresh libreoffice-fresh-de hunspell-de mpc mpd mpv cmatrix asciiquarium notepadqq thunar thunar-archive-plugin pavucontrol lxappearance qt5ct
+pacman --noconfirm -S linux-headers efibootmgr dosfstools gptfdisk ntfs-3g os-prober grub grub-btrfs networkmanager network-manager-applet nm-connection-editor firefox firefox-i18n-de thunderbird thunderbird-i18n-de acpid acpi acpi_call dbus dialog wpa_supplicant mtools avahi nfs-utils inetutils dnsmasq openbsd-netcat ipset firewalld flatpak sof-firmware dnsutils xdg-desktop-portal-wlr xdg-user-dirs xdg-utils gvfs gvfs-smb bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion terminus-font htop neofetch mc zip unzip xarchiver p7zip nss-mdns exa bat duf xorg xorg-xinit xclip xf86-video-amdgpu xf86-video-nouveau xf86-video-intel xf86-video-qxl brightnessctl pacman-contrib inxi lvm2 wget git gcc ruby go xorg-server libreoffice-fresh libreoffice-fresh-de hunspell-de mpc mpd mpv cmatrix asciiquarium notepadqq thunar thunar-archive-plugin pavucontrol lxappearance qt5ct xfce4 xfce4-goodies sddm
 
 # ------------------------------------------------------
 # set language to de_DE.UTF-8
@@ -53,7 +49,6 @@ echo "LANG=de_DE.UTF-8" >> /etc/locale.conf
 # ------------------------------------------------------
 echo "KEYMAP=$keyboardlayout" >> /etc/vconsole.conf
 echo "FONT=ter-120b" >> /etc/vconsole.conf
-
 
 # ------------------------------------------------------
 # Set hostname and localhost
@@ -116,8 +111,30 @@ echo "After:  %wheel ALL=(ALL:ALL) ALL"
 echo ""
 read -p "Open sudoers now?" c
 EDITOR=nano sudo -E visudo
-# usermod -aG wheel $username
-
+clear
+echo
+echo "Creating sddm.conf in /etc"
+echo
+sddm --example-config >> /etc/sddm.conf
+echo "Done."
+echo "Downloading and copying "
+echo "sugar-candy theme to /usr/share/sddm/themes"
+git clone https://www.github.com/quadraserver/sddm.git
+cd sddm
+chmod +x install-theme.sh
+./install-theme.sh
+cd ..
+echo 
+echo "Done."
+echo
+echo " Please change theme in sddm.conf to "
+echo " -->sugar-candy<-- and save it!"
+nano /etc/sddm.conf
+echo
+echo "Done."
+echo
+echo "Activating sddm on next reboot now..."
+systemctl enable sddm
 # ------------------------------------------------------
 # Copy installation scripts to home directory 
 # ------------------------------------------------------
@@ -129,7 +146,7 @@ cp /archinstall/6-preload.sh /home/$username/scripts
 cp /archinstall/7-kvm.sh /home/$username/scripts
 cp /archinstall/snapshot.sh /home/$username/scripts
 cp /archinstall/snapshot.sh /home/$username
-chown -R /home/$username/scripts
+chown -R $username /home/$username/scripts
 
 # ------------------------------------------------------
 # Clear archinstall directory in / 
@@ -145,7 +162,7 @@ echo "- yay: 3-yay.sh"
 echo "- zram swap: 4-zram.sh"
 echo "- timeshift snapshot tool: 5-timeshift.sh"
 echo "- preload application cache: 6-preload.sh"
-echo "- KVM-virtualisation script: 7-kvm.sh"
+#echo "- KVM-virtualisation script: 7-kvm.sh"
 echo "- snapshot.sh"
 echo ""
 echo "Please reboot."
